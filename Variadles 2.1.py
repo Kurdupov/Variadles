e1e2b19d72cf4e43660a95ef0a8ff44f1aeaf717
aleks.k <kurdupov.o@donnu.edu.ua>
2020-02-18 20:44:55 +0300

    Variadles

---
+++ Variadles 2.1.py
@@ -0,0 +1,4 @@
+a = 'Alex'
+b = 'Kurdupov'
+c = input('Введіть день: ')
+print('Good day {0} {1}! {2} is perfect day to learn some python.:)' .format(a,b,c))
---
+++ Variadles 2.py
@@ -0,0 +1,3 @@
+a = input('Введіть ім*я: ')
+b = input('Введіть день: ')
+print('Good day {0}! {1} is perfect day to learn some python.:)' .format(a,b))
